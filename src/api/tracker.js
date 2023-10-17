af3ba85e45eecd908098d96b862925c9e050383a
tonybaca <antoniocdebaca@gmail.com>
2023-10-17 11:22:52 -0600

    first commit to make this branch public

---
+++ src/api/tracker.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const instance = axios.create({
+  baseURL: "https://9d48-65-129-7-16.ngrok-free.app",
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
+import { Ionicons } from "@expo/vector-icons";
+
+const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text style={{ fontSize: 48 }}>Account Screen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
+  );
+};
+
+AccountScreen.navigationOptions = {
+  title: "Account",
+  tabBarIcon: <Ionicons name="settings" size={24} color="black" />,
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
